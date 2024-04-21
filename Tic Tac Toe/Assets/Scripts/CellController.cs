f0111d5ae87f2814f8c75a2d8c6b2e3da034f8a1
KARACb234 <medok2601@gmail.com>
2024-04-21 15:50:28 +0300

    Create cell system

--- Tic Tac Toe/Assets/Scripts/CellController.cs
+++ Tic Tac Toe/Assets/Scripts/CellController.cs
@@ -7,6 +7,13 @@ public class CellController : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
     [SerializeField]
     private Sprite[] _sprites;
+    [SerializeField]
+    private GameManager _gameManager;
+    private CellStatus _currentCellStatus;
+    [SerializeField]
+    private int col;
+    [SerializeField]
+    private int row;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +28,26 @@ public class CellController : MonoBehaviour
 
     private void OnMouseDown()
     {
-        
+        if (_currentCellStatus == CellStatus.None)
+        {
+            SetCurrentCellStatus();
+        }
+    }
+
+    private void SetCurrentCellStatus()
+    {
+        CellStatus nextCellStatus = _gameManager.GetNextCellStatus();
+        if (nextCellStatus == CellStatus.Circle)
+        {
+            _spriteRenderer.sprite = _sprites[1];
+        }
+        if (nextCellStatus == CellStatus.Cross)
+        {
+            _spriteRenderer.sprite = _sprites[0];
+        }
+        _gameManager.UpdateNextCellStatus(nextCellStatus);
+        _currentCellStatus = nextCellStatus;
+        _gameManager.SetCellStatus(row, col, _currentCellStatus);
+        _gameManager.CheckWin();
     }
 }
--- Tic Tac Toe/Assets/Scripts/GameManager.cs
+++ Tic Tac Toe/Assets/Scripts/GameManager.cs
@@ -4,33 +4,94 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private CellStatus _cellStatus;
-    
+    private CellStatus _nextCellStatus;
+    private CellStatus[,] _board = new CellStatus[3, 3];
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    public void UpdateNextCellStatus(CellStatus currentCellStatus)
     {
-        
+        if(currentCellStatus == CellStatus.Cross)
+        {
+            _nextCellStatus = CellStatus.Circle;
+        }
+        if (currentCellStatus == CellStatus.Circle)
+        {
+            _nextCellStatus = CellStatus.Cross;
+
+        }
+        if (currentCellStatus == CellStatus.None)
+        {
+            _nextCellStatus = CellStatus.Cross;
+        }
     }
     public CellStatus GetNextCellStatus()
     {
-        if(_cellStatus == CellStatus.None)
+        return _nextCellStatus;
+    }
+    public void CheckWin()
+    {
+    Debug.Log(_board[0, 0]);
+        //проверка строк
+        for(int row  = 0; row < 3; row++)
         {
-            return CellStatus.Cross;
+            if(_board[row, 0] == CellStatus.Cross &&
+                _board[row, 1] == CellStatus.Cross &&
+                _board[row, 2] == CellStatus.Cross)
+            {
+                WinCross();
+            }
+
         }
-        if (_cellStatus == CellStatus.Cross)
+        for (int row = 0; row < 3; row++)
         {
-            return CellStatus.Circle;
+            if (_board[row, 0] == CellStatus.Circle &&
+                _board[row, 1] == CellStatus.Circle &&
+                _board[row, 2] == CellStatus.Circle)
+            {
+                WinCircle();
+            }
         }
-        if (_cellStatus == CellStatus.Circle)
+        //проверка столбцов
+        for (int col = 0; col < 3; col++)
         {
-            return CellStatus.Cross;
+            if (_board[col, 0] == CellStatus.Cross &&
+                _board[col, 1] == CellStatus.Cross &&
+                _board[col, 2] == CellStatus.Cross)
+            {
+                WinCross();
+            }
+
         }
-        return _cellStatus;
+        for (int col = 0; col < 3; col++)
+        {
+            if (_board[col, 0] == CellStatus.Circle &&
+                _board[col, 1] == CellStatus.Circle &&
+                _board[col, 2] == CellStatus.Circle)
+            {
+                WinCircle();
+            }
+        }
+
+    }
+    public void SetCellStatus(int row, int col, CellStatus cellStatus)
+    {
+        _board[row, col] = cellStatus;
+    }
+    private void WinCircle()
+    {
+        Debug.Log("Win Circle");
+    }
+    private void WinCross()
+    {
+        Debug.Log("Win Cross");
+    }
+    private void Draft()
+    {
+
     }
 }
 
